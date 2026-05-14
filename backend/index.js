520201b4a9f0f74c155a4b61a8a645088a11456f
LavioSitanggang <laviositanggang@gmail.com>
2026-05-14 11:11:57 +0700

    feat: implement ACID transaction for stock mutation and UI (PBI-20)

--- backend/index.js
+++ backend/index.js
@@ -12,6 +12,7 @@ const posRoutes = require('./routes/posRoutes');
 const expenseRoutes = require('./routes/expenseRoutes');
 const reportRoutes = require('./routes/reportRoutes');
 const branchRoutes = require('./routes/branchRoutes');
+const mutationRoutes = require('./routes/mutationRoutes');
 const { verifyToken } = require('./middlewares/authMiddleware');
 const supplierRoutes = require('./routes/supplierRoutes');
 
@@ -36,6 +37,7 @@ app.use('/api/attendance', attendanceRoutes);
 app.use('/api/customers', verifyToken, customerRoutes);
 app.use('/api/pos', verifyToken, posRoutes);
 app.use('/api/branches', branchRoutes);
+app.use('/api/mutations', verifyToken, mutationRoutes);
 app.use('/api/expenses', verifyToken, expenseRoutes);
 app.use('/api/reports', verifyToken, reportRoutes);
 app.use('/api/suppliers', supplierRoutes);
